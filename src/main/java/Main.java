801a0ab3a11eff05124c5ab1936627d1833852b4
Brody <brody.broderickharris14@gmail.com>
2025-11-02 19:56:21 -0700

    Add the basic webapp

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+public class Main extends AbstractVerticle {
+  @Override
+  public void start() {
+    // Create a router
+    Router router = Router.router(vertx);
+
+    // Define a route
+    router.get("/test").handler(ctx -> {
+      ctx.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello!");
+    });
+
+    // Start an HTTP server
+    vertx.createHttpServer()
+        .requestHandler(router)
+        .listen(8080, http -> {
+          if (http.succeeded()) {
+            System.out.println("HTTP server started on port 8080");
+          } else {
+            System.err.println("Failed to start HTTP server: " + http.cause());
+          }
+        });
+  }
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new Main());
+  }
+}
